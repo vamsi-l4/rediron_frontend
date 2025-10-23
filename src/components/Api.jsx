932739636c9187661637a2fa8cd1f211859f325a
vamsi-l4 <krishnavamsi3400@gmail.com>
2025-10-23 18:35:42 +0530

    DNS issues

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:8000";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://rediron-backend-1.onrender.com";
 
 export function makeAbsolute(url) {
   if (!url) return null;
